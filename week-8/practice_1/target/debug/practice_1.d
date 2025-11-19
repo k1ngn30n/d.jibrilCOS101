61ff108cc1c9b0c8587e7c7cbc97dee7818fea3f
k1ngn30n <david.jibril@pau.edu.ng>
2025-11-19 23:57:53 +0100

    (Using Vectors and Tuples properly)

---
+++ week-8/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\USER\d.jibrilCOS101\week-8\practice_1\target\debug\practice_1.exe: C:\Users\USER\d.jibrilCOS101\week-8\practice_1\src\main.rs
---
+++ week-8/practice_11/target/debug/practice_11.d
@@ -0,0 +1,1 @@
+C:\Users\USER\d.jibrilCOS101\week-8\practice_11\target\debug\practice_11.exe: C:\Users\USER\d.jibrilCOS101\week-8\practice_11\src\main.rs
